b22d0a001ce49d7b0d45712ebc40e221c20d1797
Rahul Yadav <rahulyadav.cs1@gmail.com>
2021-05-21 21:35:07 +0530

    Update config_paytm.php
    Co-Authored-By: Rahul Yadav <78354230+rahulyadav51@users.noreply.github.com>

--- PG/lib/config_paytm.php
+++ PG/lib/config_paytm.php
@@ -8,8 +8,8 @@
 */
 
 define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
-define('PAYTM_MERCHANT_KEY', '&MWLij7_9D1eqhKx'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'CvDQtY96857949031384'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_KEY', '*******'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', '*********'); //Change this constant's value with MID (Merchant ID) received from Paytm.
 define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
